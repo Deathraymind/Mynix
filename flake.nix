8538cfd25904310cafc2d19c18be1c440c389b87
Deathraymind <deathraymind@gmail.com>
2024-05-07 09:15:57 +0200

    test

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
+      # the `inputs.nixpkgs` of the current flake,s
       # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
