fe23c166106a08c5a06a522a83bf1874fada1b2d
Deathraymind <deathraymind@gmail.com>
2024-05-11 11:10:29 +0200

    init

--- configuration.nix
+++ configuration.nix
@@ -200,7 +200,7 @@ nixpkgs.config.packageOverrides = pkgs: {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
+  }; 
  
 
 
